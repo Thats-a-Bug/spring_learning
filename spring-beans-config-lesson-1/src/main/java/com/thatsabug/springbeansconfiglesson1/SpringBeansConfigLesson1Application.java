9183d43fb8cf8f7d16bbb44bfc7238340579eda8
JoaoGFarias <jgfarias42@gmail.com>
2019-11-10 11:06:00 +0100

    Lesson 1 - Spring Boot starter

---
+++ spring-beans-config-lesson-1/src/main/java/com/thatsabug/springbeansconfiglesson1/SpringBeansConfigLesson1Application.java
@@ -0,0 +1,13 @@
+package com.thatsabug.springbeansconfiglesson1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeansConfigLesson1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeansConfigLesson1Application.class, args);
+	}
+
+}
