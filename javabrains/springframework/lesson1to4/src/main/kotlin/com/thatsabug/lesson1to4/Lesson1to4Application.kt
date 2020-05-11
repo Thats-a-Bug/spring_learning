b5d1359eaf385714ae1e7d53f8a14bdaa0993c4e
JoaoGFarias <joao@thatsabug.com>
2020-05-11 20:38:24 +0200

    Lesson 11 - Scopes

--- javabrains/springframework/lesson1to4/src/main/kotlin/com/thatsabug/lesson1to4/Lesson1to4Application.kt
+++ javabrains/springframework/lesson1to4/src/main/kotlin/com/thatsabug/lesson1to4/Lesson1to4Application.kt
@@ -14,16 +14,28 @@ fun main() {
 
 fun `print the triangle`() {
 	val context = `get context`("triangle.xml")
-	printTriangle(context)
-	printPolygon(context)
+	`print triangle`(context)
+	`print polygon`(context)
+	`verify prototype triangle`(context)
 }
 
-fun printPolygon(context: ClassPathXmlApplicationContext) {
+fun `verify prototype triangle`(context: ClassPathXmlApplicationContext) {
+	val triangle1 = context.getBean("trianglePrototype") as Triangle
+	val triangle2 = context.getBean("trianglePrototype") as Triangle
+	check(triangle1 != triangle2)
+
+	val triangle3 = context.getBean("triangle") as Triangle
+	val triangle4 = context.getBean("triangle") as Triangle
+	check(triangle3 == triangle4)
+
+}
+
+fun `print polygon`(context: ClassPathXmlApplicationContext) {
 	val obj = context.getBean("polygon") as Polygon
 	println(obj)
 }
 
-private fun printTriangle(context: ClassPathXmlApplicationContext) {
+fun `print triangle`(context: ClassPathXmlApplicationContext) {
 	val obj = context.getBean("triangle") as Triangle
 	println(obj.getCoordinates())
 	println(obj.getType())
